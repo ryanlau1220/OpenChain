12d1645db7dc0f7553837a560d0d1a252d757c8a
ryanlau1220 <liujunhong20@gmail.com>
2026-08-06 19:19:17 +0800

    configure vitest for web app frontend test

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 3000,
-		host: true,
+	test: {
+		environment: 'jsdom',
+		globals: true,
 	},
 });
