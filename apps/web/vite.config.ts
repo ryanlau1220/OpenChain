e69c5f5d222543d09921d4a155b2d2fef66b1a7d
ryanlau1220 <liujunhong20@gmail.com>
2026-08-06 19:31:11 +0800

    configure rollup vendor code-splitting for build dist

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,4 +7,24 @@ export default defineConfig({
 		port: 3000,
 		host: true,
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('cytoscape')) {
+							return 'vendor-cytoscape';
+						}
+						if (id.includes('react') || id.includes('react-dom')) {
+							return 'vendor-react';
+						}
+						if (id.includes('lucide-react')) {
+							return 'vendor-icons';
+						}
+						return 'vendor';
+					}
+				},
+			},
+		},
+	},
 });
