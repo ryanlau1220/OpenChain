0db1cee03dcf727b46434078705acdf5e472ec4c
ryanlau1220 <liujunhong20@gmail.com>
2026-08-21 17:14:03 +0800

    remove incompatible vitest react plugin

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,8 +1,6 @@
-import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [viteReact()],
 	test: {
 		environment: 'jsdom',
 		globals: true,
